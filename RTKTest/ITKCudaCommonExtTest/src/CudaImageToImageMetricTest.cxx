3d90b51cbde95449f43fec0f98ef02cf6095464e
Gordian Kabelitz <kabcode@gmail.com>
2020-01-18 15:21:17 +0100

    WIP external Testing for new ITKCudaCommon classes

---
+++ RTKTest/ITKCudaCommonExtTest/src/CudaImageToImageMetricTest.cxx
@@ -0,0 +1,20 @@
+
+
+#include "itkCudaImageToImageMetric.h"
+
+using Pixeltype = float;
+const unsigned int Dimension = 3;
+
+using CudaImageType = itk::CudaImage<Pixeltype, Dimension>;
+
+int main(int argc, char* argv[])
+{
+    auto CudaImageToImageMetric = itk::CudaImageToImageMetric<CudaImageType, CudaImageType>::New();
+
+    auto FixedImage = CudaImageType::New();
+    auto MovingImage = CudaImageType::New();
+
+    std::cout << CudaImageToImageMetric->GetNameOfClass() << std::endl;
+
+    return EXIT_SUCCESS
+}
