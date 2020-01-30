fb356c9f8d8e0fcd33c659c3a4a15c313f44d9af
KabCode <kabcode@gmail.com>
2020-01-30 11:00:48 +0100

    Test the new CudaGradientImageFilter class
    This test were here because the ITKCudaCommonDriver wasnt able to build and run the test. That changed and is therefore moved to the ITKCudaCommonTests

--- RTKTest/RTKExtension/src/CudaGradientImageFilterTest.cxx
+++ RTKTest/RTKExtension/src/CudaGradientImageFilterTest.cxx
@@ -1,20 +1,40 @@
 
+#include "rtkCudaGradientImageFilter.h"
+#include <itkCudaImage.h>
+#include "itkGradientImageFilter.h"
+#include "rtkAdditiveGaussianNoiseImageFilter.h"
 
-#include "itkCudaGradientImageFilter.h"
+#include "rtkCudaCropImageFilter.h"
+#include "rtkCudaForwardProjectionImageFilter.h"
+
 
 using Pixeltype = float;
 const unsigned int Dimension = 3;
 
 using CudaImageType = itk::CudaImage<Pixeltype, Dimension>;
+using GradientImageFilterType = itk::GradientImageFilter < CudaImageType, float, float>;
 
 int main(int argc, char* argv[])
 {
-    auto CudaGradientImageFilter = itk::CudaGradientImageFilter<CudaImageType>::New();
+    auto GradientImageFilter = GradientImageFilterType::New();
+
+    auto f = rtk::AdditiveGaussianNoiseImageFilter<CudaImageType>::New();
+    f->SetMean(2.0);
 
     auto FixedImage = CudaImageType::New();
     auto MovingImage = CudaImageType::New();
 
-    std::cout << CudaGradientImageFilter->GetNameOfClass() << std::endl;
+    auto c = rtk::CudaCropImageFilter::New();
+    c->SetInput(FixedImage);
+
+    auto p = rtk::CudaForwardProjectionImageFilter<>::New();
+    p->SetInput(FixedImage);
+
+    auto g = rtk::CudaGradientImageFilter<>::New();
+    g->SetInput(FixedImage);
+
+
+    std::cout << g->GetNameOfClass() << std::endl;
 
     return EXIT_SUCCESS;
 }
