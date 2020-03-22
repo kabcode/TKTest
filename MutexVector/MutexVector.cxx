4aac9eac504eec6fbd8b909c52d6d455aa1c1ebc
KabCode <kabcode@gmail.com>
2020-03-22 02:03:44 +0100

    Update MutexVector.cxx

--- MutexVector/MutexVector.cxx
+++ MutexVector/MutexVector.cxx
@@ -7,14 +7,16 @@
 class locker
 {
 	std::vector<std::thread> threads;
-	std::vector<int> number{0,0,0};
+	std::vector<int> number{ 0,0,0 };
 	std::vector<std::unique_lock<std::mutex>> vloc;
-	std::vector<std::mutex> mut{3};
+	std::mutex mut;
+	std::mutex* mptr;
 
 public:
 	locker()
 	{
-		InitializeLocks();
+		//InitializeLocks();
+		mptr = &mut;
 	}
 
 	void RunThreads(int n)
@@ -23,24 +25,45 @@ public:
 		{
 			threads.push_back(std::thread{ [=] { InsideThread(i); } });
 		}
-			
+	}
+
+	void RunAgain(int n)
+	{
+
+		for (int i = 0; i < n; ++i)
+		{
+			threads.push_back(std::thread{ [=] { OutsideThread(i); } });
+		}
+
 	}
 
 	void InsideThread(int n)
 	{
 		try
 		{
-			vloc[n].lock();
+			mptr->lock();
 			std::cout << std::this_thread::get_id() << std::endl;
 			this->number[n] = this->number[n] + n;
 			std::this_thread::sleep_for(std::chrono::seconds(1));
-			vloc[n].unlock();
 		}
 		catch (const std::exception& e)
 		{
 			e.what();
 		}
-		
+	}
+
+	void OutsideThread(int n)
+	{
+		try
+		{
+			this->number[n] = this->number[n] + n;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			mptr->unlock();
+		}
+		catch (const std::exception& e)
+		{
+			e.what();
+		}
 	}
 
 	void JoinThreads()
@@ -49,15 +72,12 @@ public:
 		{
 			threads[i].join();
 		}
-		for (int i = 0; i < number.size(); ++i)
-		{
-			std::cout << number[i] << std::endl;
-		}	
+		threads.clear();
 	}
 
 	void InitializeLocks()
 	{
-		for (auto& m : mut)
+		/*for (auto& m : mut)
 		{
 			vloc.push_back(std::unique_lock<std::mutex>(m));
 		}
@@ -65,15 +85,17 @@ public:
 		{
 			std::cout << "Locked!" << std::endl;
 			v.unlock();
-		}
+		}*/
 	}
 };
 
 int main()
 {
-	
+
 	auto loc = new locker();
 	loc->RunThreads(3);
+	loc->JoinThreads();
+	loc->RunAgain(3);
 	loc->JoinThreads();
 	delete loc;
 
